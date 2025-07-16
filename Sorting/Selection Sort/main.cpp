5eac6c9d1e082bf9550d02b65b52fdac71445094
Ryan Hans <primaryryanhans@gmail.com>
2025-07-16 23:42:08 +0530

    Adding Selection Sort

---
+++ Sorting/Selection Sort/main.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+    return;
+}
+
+void insertion_sort(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j])
+        {
+            swap(arr[j - 1], arr[j]);
+            j--;
+        }
+    }
+    return;
+}
+
+int main()
+{
+    cout << "Enter the number of elements: ";
+    int n;
+    cin >> n;
+    cout << endl;
+    cout << "Enter the elements: ";
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    insertion_sort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
