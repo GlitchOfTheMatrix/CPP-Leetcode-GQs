37902245ce47b28f91a2751246db42408cdf5c2e
Ryan Hans <primaryryanhans@gmail.com>
2025-07-16 23:34:08 +0530

    Adding Bubble Sort

---
+++ Sorting/Bubble Sort/main.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void bubble_sort(int arr[], int n)
+{
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = 0; j <= i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+    return;
+}
+
+void recursive_bbs(int arr[], int n)
+{
+    if (n >= 0)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+        recursive_bbs(arr, n - 1);
+    }
+    return;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    bubble_sort(arr, n);
+    recursive_bbs(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
